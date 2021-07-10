0c28d18a5ec46499f9a05992bc8cba9eec2ea69a
Rishabh Chaudhary <r.chaudhary17114@gmail.com>
2021-07-10 18:55:18 -0400

    changed the location of registered users display to home page, created profile image section for user to see on account.html, the image display is not dynamic yet

--- js/account.js
+++ js/account.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const email = document.getElementById("email");
     const password = document.getElementById("password");
     const update = document.getElementById("update");
-    const list = document.querySelector('#userList'); // variable to access the ul from account.html with the id = userList
+    const profileimg = document.querySelector(".profile-image");
 
     const db = firebase.firestore();
     let userRef = null;
@@ -43,38 +43,6 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location = "login.html";
       }
     });
-    // displaying the data of registered users on with contact information.
-    //------------------------------------------------------------------------------------------------------------------------------------
-    // makeList method is creating list items and displaying the registered users with contact info and profession
-    const makeList = (doc) =>{
-      let tr = document.createElement('tr');
-      let name = document.createElement('td');
-      let phone = document.createElement('td');
-      let profession = document.createElement('td');
-
-
-      tr.setAttribute('user-id', doc.id);
-      tr.className = 'userInfo';
-      name.className = 'userName';
-      phone.className = 'userPhone';
-      profession.className = 'userProfession';
-      name.textContent = doc.data().firstname+" "+doc.data().lastname;
-      phone.textContent = doc.data().Tel;
-      profession.textContent = doc.data().Profession[0];  
-
-      tr.appendChild(name);
-      tr.appendChild(phone);
-      tr.appendChild(profession);
-
-      list.appendChild(tr);
-    }
-    // getting the data from collection 'Users' from firestore to display on the account.html page
-    db.collection('Users').get().then((snapshot)=>{
-      snapshot.docs.forEach(doc =>{
-        makeList(doc);
-      })
-    })
-    //------------------------------------------------------------------------------------------------------------------------------------
   });
 
 
--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const auth = document.getElementById("auth")
     const noAuth = document.getElementById("no-auth")
+    const tableView = document.querySelector('.userDisp');
+    const list = document.querySelector('#userList'); // variable to access the table from index.html with the id = userList
+    const db = firebase.firestore();
 
     firebase.auth().onAuthStateChanged(function (user) {
       console.log("user", user);
@@ -10,9 +13,44 @@ document.addEventListener("DOMContentLoaded", function () {
       if (user) {
         auth.style.display = "block";
         noAuth.style.display = "none";
+        tableView.style.display = 'table';
+        // displaying the data of registered users on with contact information.
+        //------------------------------------------------------------------------------------------------------------------------------------
+        // makeList method is creating list items and displaying the registered users with contact info and profession
+        const makeList = (doc) =>{
+          let tr = document.createElement('tr');
+          let name = document.createElement('td');
+          let phone = document.createElement('td');
+          let profession = document.createElement('td');
+
+
+          tr.setAttribute('user-id', doc.id);
+          tr.className = 'userInfo';
+          name.className = 'userName';
+          phone.className = 'userPhone';
+          profession.className = 'userProfession';
+          name.textContent = doc.data().firstname+" "+doc.data().lastname;
+          phone.textContent = doc.data().Tel;
+          profession.textContent = doc.data().Profession[0];  
+
+          tr.appendChild(name);
+          tr.appendChild(phone);
+          tr.appendChild(profession);
+
+          list.appendChild(tr);
+        }
+        // getting the data from collection 'Users' from firestore to display on the account.html page
+        db.collection('Users').get().then((snapshot)=>{
+          snapshot.docs.forEach(doc =>{
+            makeList(doc);
+          })
+        })
+        //------------------------------------------------------------------------------------------------------------------------------------
+
       } else {
         auth.style.display = "none";
         noAuth.style.display = "block";
+        tableView.style.display = 'none';
       }
     });
 });
